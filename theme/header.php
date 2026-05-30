1c9aba6d3cc15a716d8eea4ca6e4b12f89aa7e56
Kavindu9 <kavinduliyo@gmail.com>
2026-05-30 16:25:24 +0500

    services page

--- theme/header.php
+++ theme/header.php
@@ -29,6 +29,28 @@
 
 <?php wp_body_open(); ?>
 
+<style>
+
+	.carousel-cell {
+	width: 28%;
+	height: 200px;
+	margin-right: 10px;
+	background: #8C8;
+	border-radius: 5px;
+	counter-increment: carousel-cell;
+	}
+
+	/* cell number */
+	.carousel-cell:before {
+	display: block;
+	text-align: center;
+	content: counter(carousel-cell);
+	line-height: 200px;
+	font-size: 80px;
+	color: white;
+	}
+</style>
+
 <div id="page">
 	<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'travel-republiq' ); ?></a>
 
