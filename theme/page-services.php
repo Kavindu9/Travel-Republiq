75b1f33f1f16d5449447695b4ea1c6524083427d
Kavindu9 <kavinduliyo@gmail.com>
2026-05-30 16:04:28 +0500

    services page

--- theme/page-services.php
+++ theme/page-services.php
@@ -22,15 +22,15 @@
                         ?>
 
                     <div class="swiper-slide">
-                        <div class="flex flex-col items-center text-center px-6 py-8 bg-[#e3eded] rounded-[28px]" style="background: #e3eded;box-shadow: 0 2px 12px rgba(36, 132, 107, 0.09);">
-                            <?php if ($image): ?>
-                                <div style="background:linear-gradient(#16717B,#80915A); width:66px; height:66px; display:flex; align-items:center; justify-content:center; border-radius:50%; margin:0 auto 12px auto;" bis_skin_checked="1">
-                                    <span style="font-size:20px; color:#fff;"><img src="<?php echo $image; ?>" alt="Icon" class="w-1/2 h-1/2 object-cover"></span>
-                                </div>
-                            <?php endif; ?>
-                            <h3 class="text-2xl font-normal text-[#1f2b3a] mb-2">
-                                <?php echo $title; ?>
-                            </h3>
+                        <div class="bg-neutral-primary-soft block max-w-sm border border-default rounded-base shadow-xs">
+                            <a href="#">
+                                <img class="rounded-t-base" src="<?php echo $image; ?>" alt="" />
+                            </a>
+                            <div class="p-6 text-left">
+                                <h3 class="text-2xl font-normal text-[#1f2b3a] mb-2">
+                                    <?php echo $title; ?>
+                                </h3>
+                            </div>
                         </div>
                     </div>
             <?php endwhile; ?>
