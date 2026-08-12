199b5987ed0cae161de59cefe52b5401abd139ac
Kavindu9 <kavinduliyo@gmail.com>
2026-08-12 17:12:35 +0500

    font updates

--- theme/partials/section-herovideo.php
+++ theme/partials/section-herovideo.php
@@ -1,17 +1,17 @@
-<div class="site-hero relative h-80 overflow-hidden">
+<div class="site-hero relative h-[540px] overflow-hidden">
 
 <?php 
     $video = get_field('hero_video');
 ?>
-    <video autoplay="" loop="" muted="" playsinline="" class="w-full h-80 object-cover" >
+    <video autoplay="" loop="" muted="" playsinline="" class="w-full h-[540px] object-cover" >
         <source type="video/mp4" src="<?php echo $video; ?>">
         Your browser does not support this video.
     </video>
 
 
     <div class="hero-wrap absolute inset-0 w-full h-full grid place-items-center">
-        <div class="font-title text-white container max-w-screen-xl h-[35%] w-2/4 grid place-items-center  mx-auto my-auto px-4 sm:px-2 border-t-4 border-b-4 border-white">
-            <h1 class="font-title font-normal text-center text-3xl text-white">DISCOVER MALDIVES WITH US</h1>
+        <div class="font-title text-white container max-w-screen-xl h-[20%] w-2/4 grid place-items-center  mx-auto my-auto px-4 sm:px-2 border-t-4 border-b-4 border-white">
+            <h1 class="font-title font-normal text-center text-4xl text-white">DISCOVER MALDIVES WITH US</h1>
         </div>
     </div>
 </div>
