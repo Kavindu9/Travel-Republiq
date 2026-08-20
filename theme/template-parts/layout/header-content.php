d485c35839a62be95127dccd50c1d6d12f078bd9
Kavindu9 <kavinduliyo@gmail.com>
2026-08-20 17:16:05 +0500

    site update

--- theme/template-parts/layout/header-content.php
+++ theme/template-parts/layout/header-content.php
@@ -21,7 +21,7 @@
       </div>
       
       <!-- Hamburger Button (Mobile Only) -->
-      <button id="menu-btn" class="md:hidden">
+      <button id="menu-btn" class="md:hidden text-black">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
@@ -52,7 +52,7 @@
 	#masthead {
 		transition: .5s;
 		z-index: 997;
-		padding: 15px 0 0;
+/* 		padding: 15px 0 0; */
 		top: 41px;
 		box-shadow: 0 2px 15px rgba(0, 0, 0, .1);
 	}
